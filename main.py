f60d6f6202e3c9072b41f2541a6dd6269b29c287
darthkpo <darthkpo>
2016-05-11 11:07:31 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+from random import randint
+
+print "Generador de actas v1.0 - fsociety"
+
+date = time.strftime("%d/%m/%Y")
+
+hra_str_h = int(time.strftime("%H"))
+hra_str_m = int(time.strftime("%M"))
+hra_str_s = int(time.strftime("%S"))
+
+hra_end_h = int(time.strftime("%H")) + randint(0,1)
+hra_end_m = int(time.strftime("%M")) + randint(30,55)
+hra_end_s = int(time.strftime("%S")) + randint(0,59)
+
+if hra_str_s + hra_end_s >= 59 :
+	hra_end_s = abs(hra_str_s - hra_end_s)
+	hra_end_m += 1
+
+if hra_str_m + hra_end_m >= 59 :
+	hra_end_m = abs(hra_str_m - hra_end_m)
+	hra_end_h += 1
+
+hra_str = "%02d:%02d:%02d" % (hra_str_h,hra_str_m,hra_str_s)
+hra_end = "%02d:%02d:%02d" % (hra_end_h,hra_end_m,hra_end_s)
+
+
+asistentes = [ "Nicolas Aguirre", "Facundo Winter", "Guillermo Rodriguez" ]
+if randint(0,100) > 55 :
+	asistentes.append("Maximiliano Bizera")
+
+print "FECHA: " + date
+print "HRA COMIENZO: " + hra_str
+print "HRA FINALIZACION: " + hra_end
+print "ASISTENTES: "
+for persona in asistentes:
+	print "    " + persona
